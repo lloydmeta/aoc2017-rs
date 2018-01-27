a3716b9be0b87d4775e72d545fdde66605af26d0
lloydmeta <lloydmeta@gmail.com>
2018-01-27 21:39:39 +0900

    Add a chain validity checker

--- src/day_24.rs
+++ src/day_24.rs
@@ -22,6 +22,33 @@ impl Component {
         let (s, _) = parser.easy_parse(s)?;
         Ok(s)
     }
+
+    fn is_valid_chain(components: &[Component]) -> bool {
+        fn go(current: usize, xs: &[Component]) -> bool {
+            /*
+             * Can probably collapse these into a single boolean expression, but
+             * readability would suffer, so ... LLVM do your thang
+             */
+            if let Some(next) = xs.first() {
+                // find the first port in next that matches current_left
+                if current == next.p1 {
+                    go(next.p2, &xs[1..])
+                } else if current == next.p2 {
+                    go(next.p1, &xs[1..])
+                } else {
+                    false // no match
+                }
+            } else {
+                true // reached the end, nothing to do, so we're good
+            }
+        }
+        if let Some(first) = components.first() {
+            // try both sides
+            go(first.p1, &components[1..]) || go(first.p2, &components[1..])
+        } else {
+            false
+        }
+    }
 }
 
 #[cfg(test)]
@@ -33,5 +60,68 @@ mod tests {
     fn components_parse_many_test() {
         let parsed = Component::parse_many(DAY_24_INPUT);
         assert!(parsed.is_ok())
+    }
+
+    #[test]
+    fn is_valid_chain_test() {
+        let chain_1 = vec![Component { p1: 0, p2: 1 }];
+        assert!(Component::is_valid_chain(&chain_1));
+        let chain_2 = vec![Component { p1: 0, p2: 1 }, Component { p1: 0, p2: 1 }];
+        assert!(Component::is_valid_chain(&chain_2));
+        let chain_3 = vec![Component { p1: 0, p2: 1 }, Component { p1: 10, p2: 1 }];
+        assert!(Component::is_valid_chain(&chain_3));
+        let chain_4 = vec![
+            Component { p1: 0, p2: 1 },
+            Component { p1: 10, p2: 1 },
+            Component { p1: 9, p2: 10 },
+        ];
+        assert!(Component::is_valid_chain(&chain_4));
+        let chain_5 = vec![
+            Component { p1: 0, p2: 2 },
+            Component { p1: 2, p2: 3 },
+            Component { p1: 3, p2: 4 },
+        ];
+        assert!(Component::is_valid_chain(&chain_5));
+        let chain_6 = vec![
+            Component { p1: 0, p2: 2 },
+            Component { p1: 2, p2: 3 },
+            Component { p1: 3, p2: 5 },
+        ];
+        assert!(Component::is_valid_chain(&chain_6));
+        let chain_7 = vec![
+            Component { p1: 0, p2: 2 },
+            Component { p1: 2, p2: 2 },
+            Component { p1: 2, p2: 3 },
+            Component { p1: 3, p2: 5 },
+        ];
+        assert!(Component::is_valid_chain(&chain_7));
+        let chain_8 = vec![
+            Component { p1: 0, p2: 2 },
+            Component { p1: 2, p2: 2 },
+            Component { p1: 2, p2: 3 },
+            Component { p1: 3, p2: 4 },
+        ];
+        assert!(Component::is_valid_chain(&chain_8));
+        let chain_9 = vec![
+            Component { p1: 0, p2: 2 },
+            Component { p1: 3, p2: 4 },
+            Component { p1: 2, p2: 3 },
+            Component { p1: 3, p2: 4 },
+        ];
+        assert!(!Component::is_valid_chain(&chain_9));
+        let chain_10 = vec![
+            Component { p1: 0, p2: 2 },
+            Component { p1: 2, p2: 4 },
+            Component { p1: 2, p2: 3 },
+            Component { p1: 3, p2: 4 },
+        ];
+        assert!(!Component::is_valid_chain(&chain_10));
+        let chain_11 = vec![
+            Component { p1: 2, p2: 0 },
+            Component { p1: 2, p2: 0 },
+            Component { p1: 0, p2: 3 },
+            Component { p1: 5, p2: 4 },
+        ];
+        assert!(!Component::is_valid_chain(&chain_11));
     }
 }
